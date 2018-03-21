73f6c054c653971f63977c64fd2f46e7dd2e3082
Victor Jordan <vjordan@fitec.org.br>
2018-03-21 14:11:03 -0300

    melhoria no codigo

--- map.js
+++ map.js
@@ -1,10 +1,5 @@
-var userCoords = [
-    { "lat": -36.777085401369156, "lng": 144.29195762802055 }, { "lat": -36.77880407914753, "lng": 144.29131389785698 }, 
-    { "lat": -36.779474353035674, "lng": 144.2938029878228 }, { "lat": -36.77768694297514, "lng": 144.29444671798638 }
-];
-
-var coords1 = [{"lat":-36.777085401369156,"lng":144.29195762802055},{"lat":-36.77880407914753,"lng":144.29131389785698},{"lat":-36.779474353035674,"lng":144.2938029878228},{"lat":-36.77768694297514,"lng":144.29444671798638}];
-var coords2 = [{"lat":-36.77492302640141,"lng":144.29006897747024},{"lat":-36.7762636361922,"lng":144.2894681626509},{"lat":-36.776951119298104,"lng":144.29176413356765},{"lat":-36.775266774736686,"lng":144.29255806743606}];
+var coords1 = [{ "lat": -36.777085401369156, "lng": 144.29195762802055 }, { "lat": -36.77880407914753, "lng": 144.29131389785698 }, { "lat": -36.779474353035674, "lng": 144.2938029878228 }, { "lat": -36.77768694297514, "lng": 144.29444671798638 }];
+var coords2 = [{ "lat": -36.77492302640141, "lng": 144.29006897747024 }, { "lat": -36.7762636361922, "lng": 144.2894681626509 }, { "lat": -36.776951119298104, "lng": 144.29176413356765 }, { "lat": -36.775266774736686, "lng": 144.29255806743606 }];
 
 regions = [
     {
@@ -27,19 +22,19 @@ var polyPath = [];
 var gmap_polygons = [];
 var infowindow = new google.maps.InfoWindow();
 var map;
+var markers = [];
+var poly;
 
 var TrPoly = function () {
-    var poly;
     var path;
-    var markers = [];
     polyPath = [];
-    
+
     // other region info
 
     this.initMap = function (el) {
         map = new google.maps.Map(document.getElementById(el), {
             zoom: 15,
-            center: {"lat":-36.77737633469688,"lng":144.29015579266252}, // Center the map on victoria, AUS.
+            center: { "lat": -36.77737633469688, "lng": 144.29015579266252 }, // Center the map on victoria, AUS.
             styles: [{
                 stylers: [
                     { hue: "#2f563c" },
@@ -70,7 +65,7 @@ var TrPoly = function () {
 
         // Add regions to the map, and save reference to them for later
         saveInMap(regions, gmap_polygons, map, showInfo);
-        
+
     }
 
     // Handles click events on a map, and adds a new point to the Polyline.
@@ -113,18 +108,8 @@ var TrPoly = function () {
         renderPoly();
     }
 
-    function clearAllMarkers() {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(null);
-        }
-        markers = [];
-    }
-
     jQuery("#resetPolygon").on("click", function () {
-        polyPath = [];
-        jQuery("#polyCoordinate").val("");
-        poly.setPath([]);
-        clearAllMarkers();
+        resetPolygon(poly);
     });
 
     function renderPoly() {
@@ -167,13 +152,13 @@ cc.initMap('map-canvas');
 
 var saveLatLong = document.getElementById('js-saveLatLon');
 
-saveLatLong.onclick = function() {
+saveLatLong.onclick = function () {
     console.log('Lat Lon antes de ser modificada');
     console.log(polyPath);
 
     var createdField = {
         id: regions.length + 1,
-        name:`Zone ${regions.length+1}`,
+        name: `Zone ${regions.length + 1}`,
         day: "Novo",
         color: "#FA5C43",
         coords: polyPath
@@ -205,7 +190,23 @@ function saveInMap(regions, gmap_polygons, map, showInfo) {
         // save reference
         gmap_polygons[i] = region;
     }
-    
+
+    resetPolygon(poly);
+
+}
+
+function resetPolygon(poly) {
+    polyPath = [];
+    jQuery("#polyCoordinate").val("");
+    poly.setPath([]);
+    clearAllMarkers();
+}
+
+function clearAllMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
 }
 
 function showInfo(e) {
